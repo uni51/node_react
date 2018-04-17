8742fafd3822283d888427500c9df95640572846
uni51 <uni51@users.noreply.github.com>
2018-04-17 14:09:39 +0900

    first node.js

---
+++ ch1/hello-server.js
@@ -0,0 +1,16 @@
+// httpモジュールを読み込む
+require('http')
+
+// Webサーバーを実行
+const svr = http.createServer(handler) // サーバーを生成
+svr.listen(8081) // ポート8081番で待ち受け開始
+
+// サーバにアクセスがあった時の処理
+function handler (req, res) {
+  console.log('url:', req.url)
+  console.log('method:', req.method)
+  // HTTPヘッダーを出力
+  res.writeHead(200, {'Content-Type': 'text/html'})
+  // レスポンスの本体を出力
+  res.end('<h1>Hello, World!</h1>\n')
+}
